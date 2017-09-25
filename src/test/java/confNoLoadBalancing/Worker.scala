9f8d179801f6936a928cad09f92e0d9cf3088e74
mahdi <mjaghouri@stackstate.com>
2017-09-25 17:41:15 +0200

    Back to Savina version of local calculations

--- src/test/java/confNoLoadBalancing/Worker.scala
+++ src/test/java/confNoLoadBalancing/Worker.scala
@@ -18,12 +18,17 @@ class Worker(var master: IMaster, var threshold: Int, var size: Int) extends Loc
         this.nqueensKernelSeq(board, depth)
       } else {
         val newDepth: Int = depth + 1
-        (0 until size)
-          .map(Functions.copyBoard(board, depth, _))
-          .filter(FastFunctions.boardValid(_, newDepth))
-          .foreach(b => {
+
+        var i: Int = 0
+        while (i < size) {
+          val b: Array[Int] = new Array[Int](newDepth)
+          System.arraycopy(board, 0, b, 0, depth)
+          b(depth) = i
+          if (FastFunctions.boardValid(b,newDepth)) {
             this.sendWork(b, newDepth, priority - 1)
-          })
+          }
+          i += 1
+        }
       }
     } else {
       println("solution")
@@ -37,27 +42,20 @@ class Worker(var master: IMaster, var threshold: Int, var size: Int) extends Loc
   private def nqueensKernelSeq(board: Array[Int], depth: Int): Unit = {
 //    println(s"Seq $depth")
     if (size != depth) {
-      (0 until size)
-        .map(Functions.copyBoard(board, depth, _))
-        .filter(FastFunctions.boardValid(_, depth + 1))
-        .foreach(nqueensKernelSeq(_, depth + 1))
+      val b: Array[Int] = new Array[Int](depth + 1)
+
+      var i: Int = 0
+      while (i < size) {
+        System.arraycopy(board, 0, b, 0, depth)
+        b(depth) = i
+        if (FastFunctions.boardValid(b, depth + 1)) {
+          nqueensKernelSeq(b, depth + 1)
+        }
+        i += 1
+      }
     } else {
       master.send(() => master.success(board))
     }
 //    println(s"Seq $depth is done")
   }
 }
-
-
-
-//        var i: Int = 0
-//        while (i < size) {
-//          val b: Array[Int] = new Array[Int](newDepth)
-//          System.arraycopy(board, 0, b, 0, depth)
-//          b(depth) = i
-//          if (Functions.boardValid(b, newDepth)) {
-//            println(s"new depth: $newDepth")
-//            master.send(() => master.sendWork(b, newDepth, priority - 1))
-//          }
-//          i += 1
-//        }
