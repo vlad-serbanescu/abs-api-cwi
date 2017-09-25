e0f26594c83f0b5f21e8f8bbb6153378e7a77b55
mahdi <mjaghouri@stackstate.com>
2017-09-25 17:35:01 +0200

    Back to Savina version of local calculations

--- src/test/java/coopNoLoadBalancing/Worker.scala
+++ src/test/java/coopNoLoadBalancing/Worker.scala
@@ -2,7 +2,7 @@ package coopNoLoadBalancing
 
 import abs.api.cwi
 import abs.api.cwi._
-import common.{FastFunctions, Functions}
+import common.FastFunctions
 
 class Worker(var threshold: Int, var size: Int) extends LocalActor with IWorker {
 
@@ -16,15 +16,22 @@ class Worker(var threshold: Int, var size: Int) extends LocalActor with IWorker 
 //    println(s"Par $depth $size $priority ${board.length} $this")
     if (size != depth) {
       if (depth >= threshold) {
-        ABSFuture.of(this.nqueensKernelSeq(board, depth))
+        ABSFuture.of(this.nqueensKernelSeq(board, depth).toList)
       } else {
         val newDepth: Int = depth + 1
-        val futures = (0 until size)
-          .map(Functions.copyBoard(board, depth, _))
-          .filter(FastFunctions.boardValid(_, newDepth))
-          .map(this.sendWork(_, newDepth, priority - 1))
+        var i: Int = 0
+        var futures: List[ABSFuture[List[Array[Int]]]] = List[ABSFuture[List[Array[Int]]]]()
+        while (i < size) {
+          val b: Array[Int] = new Array[Int](newDepth)
+          System.arraycopy(board, 0, b, 0, depth)
+          b(depth) = i
+          if (FastFunctions.boardValid(b,newDepth)) {
+            val fut: ABSFuture[List[Array[Int]]] = this.sendWork(b, newDepth, priority - 1)
+            futures = fut +: futures
+          }
+          i += 1
+        }
         val seqFut: ABSFuture[List[List[Array[Int]]]] = cwi.ABSFutureSugar.sequence(futures)
-//        println(s"awaiting seqFut $seqFut")
         getSpawn(seqFut, (list: List[List[Array[Int]]]) => {
           ABSFuture.of(list.flatten)
         })
@@ -34,28 +41,24 @@ class Worker(var threshold: Int, var size: Int) extends LocalActor with IWorker 
     }
   }
 
-  def nqueensKernelSeq(board: Array[Int], depth: Int): List[Array[Int]] = {
-    val outer = this
-//    println(s"Seq $depth $this")
-    val result = if (size != depth) {
-//      println(s"S1 $this")
-      (0 until size)
-        .map(Functions.copyBoard(board, depth, _))
-        .filter(FastFunctions.boardValid(_, depth + 1))
-        .flatMap(nqueensKernelSeq(_, depth + 1))
-        .toList
-
-      /*
-        .map(b => {val r = Functions.copyBoard(board, depth, b); println(s"mapped $b $r $outer"); r})
-        .filter(b => {val r = Functions.boardValid(b, depth + 1); println(s"mapped $b $r $outer"); r})
-        .flatMap(b => {val r = nqueensKernelSeq(b, depth + 1); println(s"mapped $b $r $outer"); r})
-       */
-
-    } else {
-//      println(s"solution ${board.toList} $this")
-      List(board) // solution
+  def nqueensKernelSeq(board: Array[Int], depth: Int): Vector[Array[Int]] = {
+    if (size == depth) {
+      Vector(board)
     }
-//    println(s"Seq done. $result $this")
-    result
+    else {
+      var result = Vector[Array[Int]]()
+      val b: Array[Int] = new Array[Int](depth + 1)
+
+      var i: Int = 0
+      while (i < size) {
+        System.arraycopy(board, 0, b, 0, depth)
+        b(depth) = i
+        if (FastFunctions.boardValid(b, depth + 1)) {
+          result = nqueensKernelSeq(b, depth + 1) ++ result
+        }
+        i += 1
+      }
+      result
+    }
   }
 }
