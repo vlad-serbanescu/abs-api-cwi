f2e5bb1e03b60c7d19697cdb0e057ad8377e5897
mahdi <mjaghouri@stackstate.com>
2017-10-03 16:05:18 +0200

    More in the new example with an exception thrown

--- src/test/java/exceptionhandling/MyActor.scala
+++ src/test/java/exceptionhandling/MyActor.scala
@@ -1,24 +1,34 @@
 package exceptionhandling
 
-import abs.api.cwi.{ABSFuture, LocalActor}
-import abs.api.cwi.ABSFuture.done
+import abs.api.cwi.{ABSFuture, ActorSystem, SugaredActor}
+import abs.api.cwi.ABSFutureSugar._
 
-class MyActor extends LocalActor {
+class MyActor extends SugaredActor {
   var healthy = true
 
-  def faulty: ABSFuture[Void] = {
+  def faulty: ABSFuture[Void] = absVoidMethod {
     if (this.healthy) {
       this.healthy = false
+      println("Healthy")
     } else {
+      println("Sick")
       this.healthy = true  // for next time
       throw new RuntimeException("I am not feeling well...")
     }
-    done
   }
+}
+
+object Main extends SugaredActor {
 
   def main(args: Array[String]): Unit = {
     val actor = new MyActor()
-
+    sequence(List(
+      actor.send(() => actor.faulty),
+      actor.send(() => actor.faulty),
+      actor.send(() => actor.faulty)
+    )) onSuccess (_ => absVoidMethod {
+      ActorSystem.shutdown()
+    })
   }
 
 }
