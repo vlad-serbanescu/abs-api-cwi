aaa84c1dacbb77a78f3684dfe015d23d43222d5f
mahdi <mjaghouri@stackstate.com>
2017-10-03 15:52:56 +0200

    new example skeleton

--- src/test/java/exceptionhandling/MyActor.scala
+++ src/test/java/exceptionhandling/MyActor.scala
@@ -1,5 +1,24 @@
 package exceptionhandling
 
-class MyActor {
+import abs.api.cwi.{ABSFuture, LocalActor}
+import abs.api.cwi.ABSFuture.done
+
+class MyActor extends LocalActor {
+  var healthy = true
+
+  def faulty: ABSFuture[Void] = {
+    if (this.healthy) {
+      this.healthy = false
+    } else {
+      this.healthy = true  // for next time
+      throw new RuntimeException("I am not feeling well...")
+    }
+    done
+  }
+
+  def main(args: Array[String]): Unit = {
+    val actor = new MyActor()
+
+  }
 
 }
