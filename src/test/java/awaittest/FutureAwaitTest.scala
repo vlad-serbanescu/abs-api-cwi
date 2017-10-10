4edc3a11bea96b845b9d6ebe9cc203e286d4d8b3
mahdi <mjaghouri@stackstate.com>
2017-10-10 21:52:25 +0200

    future await test

---
+++ src/test/java/awaittest/FutureAwaitTest.scala
@@ -0,0 +1,28 @@
+package awaittest
+
+import abs.api.cwi.{ActorSystem, SugaredActor, TypedActor}
+import abs.api.cwi.ABSFuture.done
+
+class FutureAwaitTest extends SugaredActor with TypedActor {
+  def m1 = messageHandler {
+    this ! m2
+  }
+
+  def m2 = messageHandler {
+    Thread.sleep(100)
+    println("sleep finished")
+    done
+  }
+}
+
+object FutureAwaitTest extends SugaredActor {
+  import abs.api.cwi.ABSFutureSugar._
+
+  def main(args: Array[String]): Unit = {
+    val actor = new FutureAwaitTest
+    actor ! actor.m1 onSuccess ( _ => absMethod {
+      println("done")
+      ActorSystem.shutdown()
+    })
+  }
+}
