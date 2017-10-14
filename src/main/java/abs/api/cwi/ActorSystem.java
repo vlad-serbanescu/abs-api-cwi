a11d9b7ecd1ab8ece9dd20bf96ad949995df6cb7
vlad <serbanescu.vlad.nicolae@gmail.com>
2017-10-15 01:56:31 +0200

    Added a few builtin absstdlib functions

--- src/main/java/abs/api/cwi/ActorSystem.java
+++ src/main/java/abs/api/cwi/ActorSystem.java
@@ -6,18 +6,29 @@ package abs.api.cwi;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ActorSystem {
 	/** The main executor. */
 	private static ExecutorService mainExecutor = Executors.newFixedThreadPool(10);
 
-	private ActorSystem() { }
+    private static AtomicInteger symbolicTime = new AtomicInteger(0);
+
+
+    private ActorSystem() { }
 
 	static void submit(Runnable task) {
 		mainExecutor.submit(task);
 	}
 
+	static void advanceTime(){
+        symbolicTime.incrementAndGet();
+    }
+
 	public static void shutdown() {
 		mainExecutor.shutdown();
 	}
+
+
+
 }
---
+++ src/main/java/abs/api/cwi/Functions.scala
@@ -0,0 +1,69 @@
+package abs.api.cwi
+
+import java.time.DateTimeException
+
+import scala.util.Random
+import scala.io.StdIn._;
+
+object Functions {
+
+  class Rational {
+     var num = 0
+     var denom = 0
+
+    def this(d: Double) {
+      this()
+      val s = String.valueOf(d)
+      val digitsDec = s.length - 1 - s.indexOf('.')
+
+      var number = d;
+      this.denom = 1
+      var i = 0
+      while (i < digitsDec) {
+        number *= 10
+        this.denom *= 10
+        i +=1
+      }
+      this.num = Math.round(d).asInstanceOf[Int]
+    }
+
+    def this(num: Int, denom: Int) {
+      this()
+      this.num = num
+      this.denom = denom
+    }
+
+    override def toString: String = String.valueOf(num) + "/" + String.valueOf(denom)
+  }
+
+  val r = new Random(System.currentTimeMillis());
+
+  def random(below: Int): Int = {
+    r.nextInt(below);
+  }
+
+  def truncate(a: Double): Int = {
+    return math.floor(a).toInt;
+  }
+
+  def numerator(a: Double): Int = {
+    new Rational().num;
+  }
+
+  def denominator(a: Double): Int = {
+    new Rational().denom;
+  }
+
+  def substr( str : String,  start : Int,  length : Int): String= {
+    str.substring(start, start+length);
+  }
+
+  def strlen( str : String): Int= {
+    str.length
+  }
+
+  def readln(): String={
+    readLine();
+  }
+
+}
