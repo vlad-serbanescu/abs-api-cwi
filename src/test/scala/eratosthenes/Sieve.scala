2ddbe0e1e5ce8539f90c9b9c9c2ff7c7c13b36f5
mahdi <mjaghouri@stackstate.com>
2017-11-07 11:11:25 +0100

    sieve example with typed actor

--- src/test/scala/eratosthenes/Sieve.scala
+++ src/test/scala/eratosthenes/Sieve.scala
@@ -5,10 +5,10 @@ import abs.api.cwi.{ActorSystem, SugaredActor, TypedActor}
 import abs.api.cwi.ABSFutureSugar._
 
 
-class Sieve(prime: Int) extends SugaredActor with TypedActor {
+class Sieve(prime: Int) extends SugaredActor with TypedActor[Sieve] {
   var next: Option[Sieve] = None
 
-  def divide(toDivide: Int): MessageHandler[(Int, Boolean)] = messageHandler {
+  def divide(toDivide: Int): Message[(Int, Boolean)] = messageHandler {
     if (toDivide % prime == 0) {
       done(toDivide -> false)
     } else {
@@ -24,11 +24,11 @@ class Sieve(prime: Int) extends SugaredActor with TypedActor {
 }
 
 
-object SieveMain extends SugaredActor with TypedActor {
+object SieveMain extends SugaredActor {
   def main(args: Array[String]): Unit = {
     val two = new Sieve(2)
     import two.divide
-    val futures = for (i <- 3 to 10000) yield {two ! divide(i)}
+    val futures = for (i <- 3 to 1000) yield {two ! divide(i)}
     sequence(futures) onSuccess { results: List[(Int, Boolean)] =>
       val primes = 2 +: results.filter(_._2).map(_._1)
       println(s"found ${primes.size} primes: " + primes.mkString("[", ", ", "]"))
