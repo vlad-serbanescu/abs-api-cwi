a2636d17f34320bef969ab7d10580c27492059c7
mahdi <mjaghouri@stackstate.com>
2017-11-21 13:40:29 +0100

    Actor FSM
    Finite State Machine

--- src/main/java/abs/api/cwi/ABSFutureSugar.scala
+++ src/main/java/abs/api/cwi/ABSFutureSugar.scala
@@ -4,9 +4,27 @@ import java.lang
 import java.util.concurrent.Callable
 import java.util.function.Supplier
 
-
 object TypedActor {
   trait MessageHandler[P <: TypedActor[P], V] extends Callable[ABSFuture[V]]
+}
+
+trait ActorFsm[A <: ActorFsm[A] with TypedActor[A]] {
+  self: A =>
+  import abs.api.cwi.TypedActor.MessageHandler
+  trait AbstractState
+  type TState <: AbstractState
+
+  def initState: TState
+
+  private var currentState: TState = initState
+
+  def stateHandler[V](fn: TState => (TState, ABSFuture[V])): MessageHandler[A, V] = new MessageHandler[A, V] {
+    override def call(): ABSFuture[V] = {
+      val (newState, output) = fn(currentState)
+      currentState = newState
+      output
+    }
+  }
 }
 
 trait TypedActor[A <: TypedActor[A]] extends LocalActor {
@@ -14,11 +32,11 @@ trait TypedActor[A <: TypedActor[A]] extends LocalActor {
   import TypedActor._
   type Message[V] = MessageHandler[A, V]
 
-  def messageHandler[V](fn: => ABSFuture[V]) = new MessageHandler[A, V] {
+  def messageHandler[V](fn: => ABSFuture[V]): MessageHandler[A, V] = new MessageHandler[A, V] {
     override def call() = fn
   }
 
-  def ![V] (message: MessageHandler[A, V]): ABSFuture[V] = this.send(message)
+  def ![V](message: MessageHandler[A, V]): ABSFuture[V] = this.send(message)
 }
 
 object SugaredActor {
@@ -37,11 +55,11 @@ abstract class SugaredActor extends LocalActor {
 
   def absMethod[T](fn: => T): ABSFuture[T] = ABSFuture.done(fn)
 
-  def absVoidMethod(fn: => Unit): ABSFuture[Void] = {fn; ABSFuture.done}
+  def absVoidMethod(fn: => Unit): ABSFuture[Void] = { fn; ABSFuture.done }
 
   def absContinuation[T](fn: => T): Callable[ABSFuture[T]] = () => ABSFuture.done(fn)
 
-  def absVoidContinuation(fn: => Unit): Callable[ABSFuture[Void]] = () => {fn; ABSFuture.done}
+  def absVoidContinuation(fn: => Unit): Callable[ABSFuture[Void]] = () => { fn; ABSFuture.done }
 
   def on(guard: => Boolean) = {
     val supplier = new Supplier[java.lang.Boolean] {
@@ -57,7 +75,7 @@ object ABSFutureSugar {
   type VoidFuture = ABSFuture[Void]
 
   // using a value class for less runtime overhead
-  implicit class ABSFutureImplicit[V] (val fut: ABSFuture[V]) extends AnyVal {
+  implicit class ABSFutureImplicit[V](val fut: ABSFuture[V]) extends AnyVal {
     def onSuccess[R](continuation: CallableGet[R, V])(implicit hostActor: LocalActor): ABSFuture[R] =
       hostActor.getSpawn(fut, continuation)
   }
